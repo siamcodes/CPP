d6555f3041b3f55c01ed7c087c759c8e0d575708
siamcodes <siamcodes@gmail.com>
2024-01-25 12:34:53 +0700

    update

--- exam1.cpp
+++ exam1.cpp
@@ -1,5 +1,33 @@
 #include "stdio.h"
+float readVolt();
+void volt(int i, int r);
 
+float v,i, r;
 main(){
-	printf("Hello Exam");
+	//printf("Hello Exam");
+	char ch;
+	printf("Input Choice:"); scanf("%c",&ch);
+	switch (ch){
+		case 'v': 
+		         printf("Volt"); 
+		         readVolt();
+				 volt(i,r); 
+				 break;
+	    case 'i': printf("Ampare"); break;
+	    case 'r': printf("Resister"); break;
+	    default: printf("Error");
+	}
+				
+}
+
+float readVolt(){
+	printf("Input I:"); scanf("%f",&i);
+	printf("Input R:"); scanf("%f",&r);
+	return i,r;
+}
+
+
+void volt(int i, int r){
+	v = i*r;
+	printf("Volt is %.2f",v);
 }
