0592c55721c34a9553540848a8539b0ccb7b2339
siamcodes <siamcodes@gmail.com>
2024-01-25 12:57:45 +0700

    Update

--- exam1.cpp
+++ exam1.cpp
@@ -2,19 +2,22 @@
 float readVolt();
 void volt(int i, int r);
 
-float v,i, r;
+float v,i, r;  //External Variable
 main(){
-	//printf("Hello Exam");
+	printf("Hello Example By Somkiat.J\n");
+	printf("\t v.Find Volt \n");
+	printf("\t i.Find Current \n");
+	printf("\t r.Find Resistance \n");
 	char ch;
 	printf("Input Choice:"); scanf("%c",&ch);
 	switch (ch){
 		case 'v': 
-		         printf("Volt"); 
+		         printf("Welcome to find Volt \n"); 
 		         readVolt();
 				 volt(i,r); 
 				 break;
-	    case 'i': printf("Ampare"); break;
-	    case 'r': printf("Resister"); break;
+	    case 'i': printf("Welcome to find Ampare"); break;
+	    case 'r': printf("Welcome to find Resister"); break;
 	    default: printf("Error");
 	}
 				
@@ -23,11 +26,11 @@ main(){
 float readVolt(){
 	printf("Input I:"); scanf("%f",&i);
 	printf("Input R:"); scanf("%f",&r);
-	return i,r;
+	return i,r;   //send i ,r 
 }
 
 
 void volt(int i, int r){
-	v = i*r;
+	v = i*r;  
 	printf("Volt is %.2f",v);
 }
